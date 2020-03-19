27cdea29113ed92fd350b14e37bdcd497844bffb
shreyashgupta <shreyashgupta88@gmail.com>
2020-03-20 03:23:09 +0530

    adding nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from 'react';
 import './App.css';
-
+import Particles from 'react-particles-js';
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <p>This is start</p>
+        <div class="nav-container">
+          <nav id = 'navbar'>
+            <div class="link-container ">
+              <a href='#' class="nav-link">Home</a>
+            </div>
+            <div class="link-container">
+              <a href='#' class="nav-link">FAQ</a>
+            </div>
+            <div class="link-container">
+              <a href='#' class="nav-link">List a Coin</a>
+            </div>
+            <div class="link-container">
+              <a href='#' class="nav-link">Contact Us</a>
+            </div>
+          </nav>
+        </div>
+        <Particles className="particles"
+          params={{
+                  "particles": {
+                  "number": {
+                  "value": 100
+                  },
+                  "size": {
+                  "value": 3
+                  } 
+                  },
+                  "interactivity": {
+                  "events": {
+                  "onhover": {
+                  "enable": false,
+                  "mode": "repulse"
+                  }
+                  }
+                  }
+                  }
+                }
+            width= "auto"
+            height="100vh" 
+          />
       </div>
       <div>
         <p>this is mid</p>
